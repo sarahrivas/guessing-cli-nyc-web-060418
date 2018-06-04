260a8374d6bebe0815bc71aa2930203020e3facd
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-04 02:45:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_response = gets.chomp
-  random_number = 1 + rand(6)
+  random_number = rand(7)
   while user_response != "exit"
     if user_response == random_number
       puts "You guessed the correct number!"
