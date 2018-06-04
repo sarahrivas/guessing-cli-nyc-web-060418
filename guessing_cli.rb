3f7d589f2913399f0e14138fa706e8636e5b97ae
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-04 02:36:41 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,5 +2,7 @@ def run_guessing_game
   user_response = gets.chomp
   random_number = (rand * 10).floor
   if user_response == random_number
-    puts 
+    puts "You guessed the correct number!"
+  else 
+    puts ""
 end
